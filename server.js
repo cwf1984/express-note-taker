ab0bb385e9baf1f9acbc1ab6310d982f85ba31e7
Vanessa Malloy <catwoman.fan.84@gmail.com>
2020-12-17 15:54:32 -0800

    working on post and delete routes

--- server.js
+++ server.js
@@ -12,59 +12,84 @@ const fs = require("fs");
 const PORT = 3000;
 const app = express();
 
-
-
 //takes query bar and codes any variables out of it requested
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //allows any type of data not just strings and arrays
 app.use(express.json());
 app.use(cors()); //allows all cors request
-// The following HTML routes should be created:
-
-const jsonHolder = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json")));
 
 
-// GET /notes - Should return the notes.html file.
-//static routes
+//routes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-
-
-
-// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-
-
-// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-//route
 app.get("/api/notes", (req, res) => {
     
+    const jsonHolder = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json")));
 
-    //move jsonHolder into this function so it reads everytime hit end point
     res.json(jsonHolder);
+
+    // return res.json(notes)
 
 })
 
-//route
+// send info back to server - creating new things on the server
 app.post("/api/notes", (req, res) => {
 
     //params - signify id #s or ways to route not pass info
-    console.log(req.params.id);
-    console.log(req.query.id);
-    console.log(req.body);
-    res.end();
+    //day 2 -13
+    //or is it req.body?
 
-})
+    let addNewNote = {
+        title: req.body.title,
+        text: req.body.text,
+        // id: req.body.id
+    }
+
+    fs.readFileSync("/db/db.json", "utf8", (req, res) => {
+        if (err) throw err;
+        
+        res.json(JSON.parse(res));
+
+        notes.push(addNewNote);
+
+        fs.writeFileSync( "/db/db.json", JSON.stringify(addNewNote, null), (err) => {
+            if (err) throw err;
+            res.send(newUserNote);
+        })
+
+
+    });
+
+});
 
 app.delete("/app/notes/:id", (req, res) => {
+
+    //access :id from req.params.id
+
+
+    //use FS module to read file and parse into real data for us to work with JSON.parse
+
+    // array splice method to remove by index - for loop 
+
+    // use the Array.filter() method to filter out the matching element
+    //2 options below
+    myArray = myArray.filer(element => element.id !== req.params.id);
+
+   myArray = myArray.filter( ({ id }) !== req.params.id);
+
+   //return any type of success message
+
 
 })
 
 
 //wild cards always last route
 app.get("*", (req, res) => {
-    //serve index.html file - sending file to the response
+
     res.sendFile(path.join(__dirname, "/public/index.html"));
+
 });
 
 
@@ -72,16 +97,6 @@ app.get("*", (req, res) => {
 app.listen(PORT, function(){
     console.log("App is listeing on port " + PORT);
 });
-
-
-
-
-// The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
-
-
-// The following API routes should be created:
-
-
 
 
 
